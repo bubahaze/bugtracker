41f1fc846a9ecc1a959bb755783a666cc617bcdb
Igor Poludnikiewicz <igor.poludnikiewicz@gmail.com>
2021-12-03 11:16:05 +0100

    enable form login auth

--- src/main/java/com/poludnikiewicz/bugtracker/api/ManagementController.java
+++ src/main/java/com/poludnikiewicz/bugtracker/api/ManagementController.java
@@ -6,6 +6,7 @@ import com.poludnikiewicz.bugtracker.service.BugService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,13 +21,15 @@ public class ManagementController {
     }
 
     @DeleteMapping("/bug/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> deleteBug(@PathVariable Long id) {
         //question mark means it will not return any type in responseEntity
         service.deleteBug(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PutMapping("/bug")
+    @PutMapping("/bug/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_STAFF')")
     public ResponseEntity<Bug> updateBug(@RequestBody Bug bug, @PathVariable Long id) {
 
         Bug updatedBug = service.updateBug(bug, id);
--- src/main/java/com/poludnikiewicz/bugtracker/security/ApplicationSecurityConfig.java
+++ src/main/java/com/poludnikiewicz/bugtracker/security/ApplicationSecurityConfig.java
@@ -5,8 +5,10 @@ import com.poludnikiewicz.bugtracker.auth.ApplicationUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +19,8 @@ import static com.poludnikiewicz.bugtracker.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+// ^^ this enables using @PreAuthorize annotation replacing mvc/antMatchers
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     //@Autowired
@@ -35,16 +39,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userService)
                 .passwordEncoder(passwordEncoder);
 
-        auth.inMemoryAuthentication()
-                .withUser("Admin").password(passwordEncoder.encode("adminPass")).roles("ADMIN")
-                //.and().withUser("User").password("userPassword").roles("USER")
-                //.and().withUser("Engineer").password("engineerPassword").roles("STAFF")
-                .and().passwordEncoder(passwordEncoder);
+//        auth.inMemoryAuthentication()
+//                .withUser("Admin").password(passwordEncoder.encode("adminPass")).roles("ADMIN")
+//                //.and().withUser("User").password("userPassword").roles("USER")
+//                //.and().withUser("Engineer").password("engineerPassword").roles("STAFF")
+//                .and().passwordEncoder(passwordEncoder);
     }
 
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("manage/api/**").hasRole(ADMIN.name())
+                //.mvcMatchers(HttpMethod.DELETE, "manage/api/**").hasRole(ADMIN.name())
+                //.mvcMatchers("manage/api/**").hasAnyRole(ADMIN.name(), STAFF.name())
                 //.mvcMatchers("/api/manage").hasRole("STAFF")
                 //.mvcMatchers("/api/bugtracker/*").hasRole("USER")
                 .mvcMatchers("/login").permitAll()
@@ -53,7 +58,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()//.headers().frameOptions().disable()
                 //.and()
                 .formLogin()
-                //.loginPage("/login.html") //NOT EXISTING YET!
+                .loginPage("/login").permitAll();
                 //.loginProcessingUrl("/perform_login")
                 //.defaultSuccessUrl("/homepage.html", false) // false =user redirected to previous page they wanted to visit before being prompted to authenticate.
                 //.failureUrl("/login.html?error=true")
@@ -63,15 +68,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                // .logoutUrl("/perform_logout")
                // .deleteCookies("JSESSIONID")
                 //.logoutSuccessHandler(logoutSuccessHandler())
-                .and()
-                .httpBasic();
 
     }
-
-//    @Bean
-//    public PasswordEncoder getEncoder() {
-//        return new BCryptPasswordEncoder();
-//    } commented because I provided password encoder as a private field
 
 //    @Bean
 //    public DaoAuthenticationProvider daoAuthenticationProvider() {
@@ -79,7 +77,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 //        provider.setPasswordEncoder(passwordEncoder);
 //        provider.setUserDetailsService(userService);
 //        return provider;
-//    }
+//    } this will be for DB authentication later on
 
 
 }
