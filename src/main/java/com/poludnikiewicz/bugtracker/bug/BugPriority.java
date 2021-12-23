d684f18c5b0d5d866520375fb18dfb8ce2968e21
Igor Poludnikiewicz <igor.poludnikiewicz@gmail.com>
2021-12-23 19:19:21 +0100

    add BugPriority enum

--- src/main/java/com/poludnikiewicz/bugtracker/bug/BugPriority.java
+++ src/main/java/com/poludnikiewicz/bugtracker/bug/BugPriority.java
@@ -1,2 +1,23 @@
-package com.poludnikiewicz.bugtracker.bug;public enum BugPriority {
+package com.poludnikiewicz.bugtracker.bug;
+
+/**
+ * P1_CRITICAL priority is of highest importance, discussed feature is crucial for maintenance of
+ * whole application, blocks development/testing or causes data loss, solution of such issue should come first, ASAP;
+ * P2_IMPORTANT priority means the bug cause major damage for application flow and should be solved possibly fast;
+ * P3_NORMAL priority - the bug blocks non-critical functionality and a workaround exists.
+ * P4_MARGINAL priority is for bugs, that do not impede application flow, can be some cosmetic issues.
+ * Solution can be delayed until some
+ * unknown future release.
+ * P5_REDUNDANT priority. If any complex works are needed in order to solve this issue it will likely be rejected
+ * It is not obligatory for a staff member to solve such issue. minor significance, cosmetic issues, low or no impact to users
+ */
+
+public enum BugPriority {
+    P1_CRITICAL,
+    P2_IMPORTANT,
+    P3_NORMAL,
+    P4_MARGINAL,
+    P5_REDUNTANT,
+    UNSET
+
 }
