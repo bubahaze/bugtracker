257ae6594772ea743ea14bf5a53cef35217f4c7e
Igor Poludnikiewicz <igor.poludnikiewicz@gmail.com>
2021-12-29 19:23:50 +0100

    add mapping to BugResponse

--- src/main/java/com/poludnikiewicz/bugtracker/bug/BugService.java
+++ src/main/java/com/poludnikiewicz/bugtracker/bug/BugService.java
@@ -1,6 +1,8 @@
 package com.poludnikiewicz.bugtracker.bug;
 
+import com.poludnikiewicz.bugtracker.auth.ApplicationUser;
 import com.poludnikiewicz.bugtracker.bug.dto.BugRequest;
+import com.poludnikiewicz.bugtracker.bug.dto.BugResponse;
 import com.poludnikiewicz.bugtracker.exception.BugNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -29,7 +32,7 @@ public class BugService {
                 .description(request.getDescription())
                 .uniqueCode(uniqueCode)
                 .status(BugStatus.REPORTED)
-                .opSystemWhereBugOccured(request.getOpSystemWhereBugOccured())
+                .opSystemWhereBugOccurred(request.getOpSystemWhereBugOccured())
                 .usernameOfReporterOfBug(reporterUsername)
                 .priority(BugPriority.UNSET)
                 .build();
@@ -43,7 +46,7 @@ public class BugService {
             bugToUpdate.setSummary(bug.getSummary());
             bugToUpdate.setProject(bug.getProject());
             bugToUpdate.setDescription(bug.getDescription());
-            bugToUpdate.setOpSystemWhereBugOccured(bug.getOpSystemWhereBugOccured());
+            bugToUpdate.setOpSystemWhereBugOccurred(bug.getOpSystemWhereBugOccurred());
             bugToUpdate.setPriority(bug.getPriority());
             bugToUpdate.setStatus(bug.getStatus());
             bugToUpdate.setAssignedStaffMember(bug.getAssignedStaffMember());
@@ -55,13 +58,15 @@ public class BugService {
         bugRepo.deleteById(id);
     }
 
-    public List<Bug> findAllBugs() {
-        return bugRepo.findAll();
-    }
-
     public Bug findById(Long id) {
         return bugRepo.findById(id)
                 .orElseThrow(() -> new BugNotFoundException("Bug with id " + id + " not found."));
+    }
+
+    public BugResponse findBugResponseById(Long id) {
+        Bug bug = bugRepo.findById(id)
+                .orElseThrow(() -> new BugNotFoundException("Bug with id " + id + " not found."));
+        return mapToBugResponse(bug);
     }
 
     public Collection<Bug> findByProject(String project) {
@@ -76,6 +81,33 @@ public class BugService {
     public Bug findByUniqueCode(String uniqueCode) {
         return bugRepo.findByUniqueCode(uniqueCode)
                 .orElseThrow(() -> new BugNotFoundException("Bug with unique code " + uniqueCode + " not found."));
+    }
+
+    public List<BugResponse> findAllBugs() {
+        return ( bugRepo
+                .findAll())
+                .stream()
+                .map(this::mapToBugResponse)
+                .collect(Collectors.toList());
+    }
+
+    private BugResponse mapToBugResponse(Bug bug) {
+        BugResponse bugResponse = new BugResponse();
+        bugResponse.setSummary(bug.getSummary());
+        bugResponse.setDescription(bug.getDescription());
+        bugResponse.setProject(bug.getProject());
+        bugResponse.setCreationDate(bug.getCreationDate());
+        bugResponse.setLastChangeAt(bug.getLastChangeAt());
+        bugResponse.setUniqueCode(bug.getUniqueCode());
+        bugResponse.setStatus(bug.getStatus());
+        bugResponse.setOpSystemWhereBugOccured(bug.getOpSystemWhereBugOccurred());
+        bugResponse.setUsernameOfReporterOfBug(bug.getUsernameOfReporterOfBug());
+        bugResponse.setPriority(bug.getPriority());
+        if (bug.getAssignedStaffMember() != null) {
+            ApplicationUser assignee = bug.getAssignedStaffMember();
+            bugResponse.setUsernameOfAssignee(assignee.getUsername());
+        }
+        return bugResponse;
     }
 
     //TODO: QUERY METHODS
