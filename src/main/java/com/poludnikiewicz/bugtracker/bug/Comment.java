1820bc668cebb9d4920d0d9b2172f6d79d94d9ef
Igor Poludnikiewicz <igor.poludnikiewicz@gmail.com>
2021-12-29 19:30:34 +0100

    add lombok annotations

--- src/main/java/com/poludnikiewicz/bugtracker/bug/Comment.java
+++ src/main/java/com/poludnikiewicz/bugtracker/bug/Comment.java
@@ -1,6 +1,7 @@
 package com.poludnikiewicz.bugtracker.bug;
 
 import com.poludnikiewicz.bugtracker.auth.ApplicationUser;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +9,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comment {
 
     @Id
