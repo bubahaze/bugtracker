d2d4d71ed293737f2b3ae2d86199f97c128f9e92
Igor Poludnikiewicz <igor.poludnikiewicz@gmail.com>
2022-01-03 18:30:01 +0100

    add query method of searching by keyword

--- src/main/java/com/poludnikiewicz/bugtracker/bug/BugRepository.java
+++ src/main/java/com/poludnikiewicz/bugtracker/bug/BugRepository.java
@@ -1,5 +1,6 @@
 package com.poludnikiewicz.bugtracker.bug;
 
+import com.poludnikiewicz.bugtracker.bug.dto.BugResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,7 +14,14 @@ public interface BugRepository extends JpaRepository<Bug, Long> {
 
     Optional<Bug> findByUniqueCode(String uniqueCode);
 
-    Collection<Bug> findByProjectIgnoreCaseOrderByCreationDateDesc(String project);
+    //@Query("select b from Bug b where b.project like '%?1%'")
+    List<Bug> findByProjectContainingIgnoreCaseOrderByLastChangeAtDesc(String project);
+
+    @Query("select b from Bug b where lower(b.summary) like lower(concat('%', ?1,'%'))" +
+            " or lower(b.description) like lower(concat('%', ?1,'%')) or lower(b.project) like" +
+            " lower(concat('%', ?1,'%')) or lower(b.opSystemWhereBugOccurred) like lower(concat('%', ?1,'%')) " +
+            "or lower(b.usernameOfReporter) like lower(concat('%', ?1,'%'))")
+    List<Bug> findByKeyword(String keyword);
 
     @Query("select b from Bug b where b.assignedStaffMember.username = ?1")
     List<Bug> findAllBugsAssignedToPrincipal(String username);
