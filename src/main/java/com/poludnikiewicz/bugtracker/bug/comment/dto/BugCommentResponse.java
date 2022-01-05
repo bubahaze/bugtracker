eb5fd6427f7758a9d45d2bb17ca6a27d95b7b8b1
Igor Poludnikiewicz <igor.poludnikiewicz@gmail.com>
2022-01-05 16:57:32 +0100

    add response dto

--- src/main/java/com/poludnikiewicz/bugtracker/bug/comment/dto/BugCommentResponse.java
+++ src/main/java/com/poludnikiewicz/bugtracker/bug/comment/dto/BugCommentResponse.java
@@ -1,2 +1,15 @@
-package com.poludnikiewicz.bugtracker.bug.comment.dto;public class BugCommentResponse {
+package com.poludnikiewicz.bugtracker.bug.comment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BugCommentResponse {
+
+    private String content;
+    private String author;
+
 }
