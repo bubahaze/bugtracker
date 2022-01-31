706b929ed12ec1dac81d4d0b26b640c756e41104
Igor Poludnikiewicz <igor.poludnikiewicz@gmail.com>
2022-01-31 19:01:31 +0100

    delete final keyword at fields to provide noArgsConstructor

--- src/main/java/com/poludnikiewicz/bugtracker/registration/RegistrationRequest.java
+++ src/main/java/com/poludnikiewicz/bugtracker/registration/RegistrationRequest.java
@@ -1,9 +1,6 @@
 package com.poludnikiewicz.bugtracker.registration;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -12,20 +9,21 @@ import javax.validation.constraints.Size;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode
 @ToString
 public class RegistrationRequest {
     @NotBlank(message = "Username must not be blank")
     @Size(min = 3, max = 20, message = "username must contain 3-20 characters")
-    private final String username;
+    private String username;
     @NotBlank(message = "First name must not be blank")
-    private final String firstName;
+    private String firstName;
     @NotBlank(message = "Last name must not be blank")
-    private final String lastName;
+    private String lastName;
     @Email(message = "Look like this is not a valid email")
     @NotBlank(message = "Email must not be blank")
-    private final String email;
+    private String email;
     @NotEmpty(message = "Password must not be empty")
     @Size(min = 8, message = "Password must contain at least 8 characters")
-    private final String password;
+    private String password;
 }
