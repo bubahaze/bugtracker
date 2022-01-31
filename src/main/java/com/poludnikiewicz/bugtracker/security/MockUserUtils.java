d21fd2b6bc0b3bb8cdf90d496bc63dc94b622839
Igor Poludnikiewicz <igor.poludnikiewicz@gmail.com>
2022-01-31 19:03:13 +0100

    create MockUserUtils class

--- src/main/java/com/poludnikiewicz/bugtracker/security/MockUserUtils.java
+++ src/main/java/com/poludnikiewicz/bugtracker/security/MockUserUtils.java
@@ -1,2 +1,16 @@
-package com.poludnikiewicz.bugtracker.security;public class MockUserUtils {
+package com.poludnikiewicz.bugtracker.security;
+
+import com.poludnikiewicz.bugtracker.auth.ApplicationUser;
+
+public class MockUserUtils {
+
+    private MockUserUtils() {}
+
+
+    public static ApplicationUser getMockUser(String username) {
+        ApplicationUser user = new ApplicationUser("mockUser", "John", "Doe",
+                "johndoe@gmail.com", "password");
+        user.setEnabled(true);
+        return user;
+    }
 }
