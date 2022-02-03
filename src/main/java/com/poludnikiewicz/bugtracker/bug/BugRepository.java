56f03e78340f179643ddaaa274509e04aef5ef59
Igor Poludnikiewicz <igor.poludnikiewicz@gmail.com>
2022-02-03 18:03:29 +0100

    refactor code

--- src/main/java/com/poludnikiewicz/bugtracker/bug/BugRepository.java
+++ src/main/java/com/poludnikiewicz/bugtracker/bug/BugRepository.java
@@ -1,13 +1,10 @@
 package com.poludnikiewicz.bugtracker.bug;
 
-import com.poludnikiewicz.bugtracker.bug.dto.BugResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface BugRepository extends JpaRepository<Bug, Long> {
