4d6b76784e4ec82c4ccbf7fcb3cc58eb67a4ab29
Igor Poludnikiewicz <igor.poludnikiewicz@gmail.com>
2022-02-10 19:21:26 +0100

    add skeleton for test class

--- src/test/java/com/poludnikiewicz/bugtracker/api/BugCommentControllerTest.java
+++ src/test/java/com/poludnikiewicz/bugtracker/api/BugCommentControllerTest.java
@@ -1,4 +1,37 @@
+package com.poludnikiewicz.bugtracker.api;
+
+import com.poludnikiewicz.bugtracker.bug.comment.BugCommentService;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@WebMvcTest(BugCommentController.class)
 class BugCommentControllerTest {
-  
+
+    @Autowired
+    MockMvc mockMvc;
+    @MockBean
+    BugCommentService commentService;
+
+    @Test
+    void postBugComment() {
+    }
+
+    @Test
+    void deleteBugComment() {
+    }
+
+    @Test
+    void updateBugComment() {
+    }
 }
