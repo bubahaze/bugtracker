4fc2d41d3d167877a417125b52b57c010e5bdf67
Igor Poludnikiewicz <igor.poludnikiewicz@gmail.com>
2022-02-21 17:20:59 +0100

    change in-memory users names

--- src/main/java/com/poludnikiewicz/bugtracker/security/ApplicationSecurityConfig.java
+++ src/main/java/com/poludnikiewicz/bugtracker/security/ApplicationSecurityConfig.java
@@ -61,10 +61,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.authenticationProvider(daoAuthenticationProvider());
 
         auth.inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder.encode("adminpass"))
+                .withUser("memoadmin").password(passwordEncoder.encode("adminpass"))
                 .roles("ADMIN")
                 .and().withUser("memouser").password(passwordEncoder.encode("userpass")).roles("USER")
-                .and().withUser("staffmem").password(passwordEncoder.encode("staffmem")).roles("STAFF");
+                .and().withUser("memostaff").password(passwordEncoder.encode("staffpass")).roles("STAFF");
     }
 
     @Bean
